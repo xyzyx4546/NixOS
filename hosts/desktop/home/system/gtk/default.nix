dbea1b235f50ff04759d8d263208b83097dc63eb
xyzyx4546 <ehd0178@gmail.com>
2024-07-07 21:31:37 +0200

    restructuring

--- hosts/desktop/home/system/gtk/default.nix
+++ hosts/desktop/home/system/gtk/default.nix
@@ -1,11 +1,21 @@
 { pkgs, ... }: {
 
   gtk = {
-    enable = true;
+    #enable = true;
 
     theme = {
-      name = "adw.gtk3";
+      name = "adw-gtk3";
       package = pkgs.adw-gtk3;
-    }
+    };
+  };
+
+  home.file = {
+    ".config/gtk-3.0/gtk.css".text = ''
+      @import url("file://~/.config/material/colors/colors-gtk.css");
+    '';
+
+    ".config/gtk-4.0/gtk.css".text = ''
+      @import url("file://~/.config/material/colors/colors-gtk.css");
+    '';
   };
 }
